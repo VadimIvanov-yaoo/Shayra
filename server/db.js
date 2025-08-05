0875c6a1b845892f779eb1795179459c365e9c03
VadimIvanov-yaoo <vi2407751@gmail.com>
2025-08-05 20:38:10 +0300

    fix backend server

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import { Sequelize } from 'sequelize'
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -9,6 +10,12 @@ const sequelize = new Sequelize(
     dialect: 'postgres',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 )
 
